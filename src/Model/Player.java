cafef5945f99f3ae2fee7bf33003b443d71de84a
CosmicParadoxDragon <sohrab.kazak@gmail.com>
2023-02-26 17:21:56 -0800

    Building structure little/no code

---
+++ src/Model/Player.java
@@ -0,0 +1,50 @@
+package Model;
+
+import java.util.Vector;
+
+public class Player {
+    int compost;
+    int soil;
+    Vector <Card> hand;
+    Vector<Vector <Card>> playerIsland;
+
+    void activePlanting()
+    {
+
+    }
+
+    void inactivePlanting()
+    {
+
+    }
+
+    void activeComposting()
+    {
+
+    }
+
+    void inactiveComposting()
+    {
+
+    }
+
+    void activeWatering()
+    {
+
+    }
+    
+    void inactiveWatering()
+    {
+
+    }
+
+    void activeGrowing()
+    {
+
+    }
+
+    void inactiveGrowing()
+    {
+
+    }
+}
